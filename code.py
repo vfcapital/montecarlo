1f43615bbbe56db924c9614344ca59c2be24b0fc
Viktor <79378770+vfcapital@users.noreply.github.com>
2022-02-18 11:52:23 +0100

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Set inpute variables
-Number_of_years = 4
+number_of_years = 4
 periods_per_year = 360
 number_of_paths = 10
 price_start = 100
@@ -11,7 +11,7 @@ volatility = 0.2
 dividend_yield = 0
 
 # Create empty path matrix
-path_matrix = np.empty((number_of_paths,Number_of_years*periods_per_year))
+path_matrix = np.empty((number_of_paths,number_of_years*periods_per_year))
 
 #Calculate risk-neutral probability
 u = np.exp(volatility*np.sqrt(1/periods_per_year))
@@ -20,14 +20,13 @@ p = (np.exp((risk_free_rate-dividend_yield)*(1/periods_per_year))-d)/(u-d)
 
 # Fill path matrix with random walks
 for path in range(number_of_paths):
-    for t in range(Number_of_years*periods_per_year):
+    for t in range(number_of_years*periods_per_year):
         if t == 0:
             path_matrix[path, t] = price_start
         else: 
             path_matrix[path, t] = path_matrix[path, t-1]*np.random.choice((u,d), p=[p,1-p])
 
 # Create plot
-        
 x_ticks = [0, 200, 560, 920, 1280]
 x_labels = ['', 'Jahr 1', 'Jahr 2', 'Jahr 3', 'Jahr 4']            
 
